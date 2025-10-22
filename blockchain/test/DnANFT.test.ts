f5224d4c9bfc42420017e6cb85ccb045ec6116aa
Your Name <you@example.com>
2025-10-22 14:59:57 +0200

    test: add successfull unit test for DnANFT minting and purchase flow

---
+++ blockchain/test/DnANFT.test.ts
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("DnANFT", function () {
+  it("should mint, set price and allow a user to buy an NFT", async function () {
+    const [owner, admin, buyer, other] = await ethers.getSigners();
+
+    const DnANFT = await ethers.getContractFactory("DnANFT");
+    const nft = await DnANFT.deploy("DnA Editorials", "DNA");
+    await nft.waitForDeployment();
+
+    await nft.connect(owner).setAdmin(admin.address, true);
+    expect(await nft.isAdmin(admin.address)).to.be.true;
+
+    await nft.connect(admin).mintTo(owner.address, 1, "ipfs://token1");
+    expect(await nft.ownerOf(1)).to.equal(owner.address);
+
+    const price = ethers.parseEther("0.5");
+    await nft.connect(admin).setTokenPrice(1, price);
+    expect(await nft.tokenPrice(1)).to.equal(price);
+
+    await expect(nft.connect(buyer).buy(1, { value: price })).to.emit(nft, "Purchased");
+
+    expect(await nft.ownerOf(1)).to.equal(buyer.address);
+
+    await nft
+      .connect(buyer)
+      ["safeTransferFrom(address,address,uint256)"](buyer.address, other.address, 1);
+
+    expect(await nft.ownerOf(1)).to.equal(other.address);
+  });
+});
